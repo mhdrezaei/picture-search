2332fd46b273e1be2eb293c19960bdc0bd49d1e5
رضائی - محمد <rezaee-moha@tehran.iri>
2022-08-11 19:15:50 +0430

    add new component and some changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import '../index.css'
+
+import SearchBar from './SearchBar'
+import SearchDetail from './SearchDetail'
 
 export class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div>
+        <SearchBar/>
+        <SearchDetail/>
+      </div>
     )
   }
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import 'boxicons/css/boxicons.css';
+
 
 export class SearchBar extends Component {
   render() {
     return (
-      <div></div>
-    )
+      <>
+        <div className="container">
+          <div className="card mt-5 p-5">
+            <div className="input-group input-group-merge">
+              <span className="input-group-text search-icon" id="basic-addon-search31">
+                <i class="bx bx-search-alt"></i>
+              </span>
+              <input
+                type="text"
+                class="form-control form-control-lg search-input"
+                placeholder="Search your picture"
+                aria-label="Search..."
+                aria-describedby="basic-addon-search31"
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
-export default SearchBar
+export default SearchBar;
