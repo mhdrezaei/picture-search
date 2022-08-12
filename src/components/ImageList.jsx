0b472cd5cc533dfac59ed3c5fc060b32c36585f6
رضائی - محمد <rezaee-moha@tehran.iri>
2022-08-12 13:23:19 +0430

    create component for listing image

---
+++ src/components/ImageList.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import ImageItem from "./image-item/ImageItem";
+
+export class ImageList extends Component {
+  render() {
+    return (
+      <div className="container my-5">
+        <div className="col-md-12">
+          <div className="grid">
+            {this.props.items.map((item) => {
+              return (
+                <ImageItem
+                  url={item.urls.small}
+                  profileImg={item.user.profile_image}
+                  userName={item.user.username}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ImageList;
